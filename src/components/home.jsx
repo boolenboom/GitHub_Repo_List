b8bf35bbe6081fed8ddc64861e70cd4ea2b8f0cc
boolenboom <alan93072@gmail.com>
2022-07-17 16:23:29 +0800

    更換封面圖片 & 更改LOGO動畫 旋轉 -> 呼吸

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 
-import logo from '../logo.svg'
+import logo from '../icon/github.svg';
+import '../stylesheets/home.scss';
 
 export default function Home(){
     return (
         <div className="home">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>GitHub Repos List</h1>
-            <Link to={'/users'}>{'-->點此閱覽使用者清單<--'}</Link>
+            <Link to={'/users'}>{'--> 點此閱覽使用者清單 <--'}</Link>
         </div>
     )
 }
